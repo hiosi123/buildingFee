c909dcec81f3c3e643c55faadae3c494403097cf
tls1641 <tls1641@trustnhope.com>
2023-10-18 00:01:00 +0900

    database

--- storage/building.go
+++ storage/building.go
@@ -1,6 +1,9 @@
 package storage
 
 import (
+	"database/sql"
+	"fmt"
+
 	"github.com/jmoiron/sqlx"
 )
 
@@ -12,7 +15,7 @@ func NewBuildingStorage(conn *sqlx.DB) *BuildingStorage {
 	return &BuildingStorage{Conn: conn}
 }
 
-type NewBuilding struct {
+type Building struct {
 	Id                   int64  `json:"id" db:"id"`
 	Name                 string `json:"name" db:"name"`
 	Owner                string `json:"owner" db:"owner"`
@@ -22,21 +25,30 @@ type NewBuilding struct {
 	Discarded            string `json:"discarded" db:"discarded" `
 }
 
-type NewFloor struct {
-	Id               int64   `json:"id" db:"id"`
-	Floor            string  `json:"floor" db:"floor"`
-	Name             string  `json:"name" db:"name"`
-	Area             string  `json:"area" db:"area"`
-	Electric_measure float32 `json:"electric_measure" db:"electric_measure"`
-	Water_measure    float32 `json:"water_measure" db:"water_measure"`
-	Building_id      int64   `json:"building_id" db:"building_id"`
+type Floor struct {
+	Id          int64  `json:"id" db:"id"`
+	Floor       string `json:"floor" db:"floor"`
+	Name        string `json:"name" db:"name"`
+	Area        string `json:"area" db:"area"`
+	Building_id int64  `json:"building_id" db:"building_id"`
 }
 
-func (b *BuildingStorage) CreateNewBuilding(data NewBuilding) (int, error) {
+type Charge struct {
+	Id               int64   `json:"id" db:"id"`
+	Year             string  `json:"year" db:"year"`
+	Date             string  `json:"date" db:"date"`
+	Electric_measure float32 `json:"electric_measure" db:"electric_measure"`
+	Water_measure    float32 `json:"water_measure" db:"water_measure"`
+	Floor_id         int64   `json:"floor_id" db:"floor_id"`
+}
 
-	res, err := b.Conn.Exec(`
-	INSERT INTO building (name, owner, number_floors, number_floors_ground, created_at) 
-	values (?, ?, ?, ?, ?)`, data.Name, data.Owner, data.Number_floors, data.Number_floors_ground, data.Created_at)
+func (b *BuildingStorage) CreateNewBuilding(data Building) (int, error) {
+	query, values, err := InsertQueryStr(data)
+	if err != nil {
+		return 0, err
+	}
+
+	res, err := b.Conn.Exec(fmt.Sprintf(`INSERT INTO building %s`, query), values...)
 	if err != nil {
 		return 0, err
 	}
@@ -47,4 +59,43 @@ func (b *BuildingStorage) CreateNewBuilding(data NewBuilding) (int, error) {
 	}
 
 	return int(id), nil
+}
+
+func (b *BuildingStorage) CreateNewFloor(data Floor) (int, error) {
+	query, values, err := InsertQueryStr(data)
+	if err != nil {
+		return 0, err
+	}
+
+	res, err := b.Conn.Exec(fmt.Sprintf(`INSERT INTO floor %s`, query), values...)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
+}
+
+func (b *BuildingStorage) GetFloor(Id int64) (Floor, error) {
+	var floor Floor
+
+	fQuery, err := GetPureFieldStr(Floor{})
+	if err != nil {
+		return Floor{}, err
+	}
+
+	query := fmt.Sprintf("SELECT %s FROM floor WHERE id = ?", fQuery)
+	err = b.Conn.Get(&floor, query, Id)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return Floor{}, nil
+		}
+		return Floor{}, err
+	}
+
+	return floor, nil
 }
--- storage/util.go
+++ storage/util.go
@@ -1,15 +1,21 @@
 package storage
 
 import (
+	"errors"
 	"fmt"
 	"reflect"
 	"strings"
 )
 
-func GetPureFieldStr(myInstance struct{}) string {
+func GetPureFieldStr(s any) (string, error) {
 	// Use reflection to iterate over struct fields and build a comma-separated string
 	var values []string
-	structValue := reflect.ValueOf(myInstance)
+	structValue := reflect.ValueOf(s)
+
+	if structValue.Kind() != reflect.Struct {
+		return "", errors.New("input must be struct")
+	}
+
 	for i := 0; i < structValue.NumField(); i++ {
 		field := structValue.Field(i)
 
@@ -20,5 +26,43 @@ func GetPureFieldStr(myInstance struct{}) string {
 	// Join the values into a comma-separated string
 	valuesStr := strings.Join(values, ",")
 
-	return valuesStr
+	return valuesStr, nil
+}
+
+func InsertQueryStr(s any) (string, []any, error) {
+	value := reflect.ValueOf(s)
+	typ := reflect.TypeOf(s)
+
+	if value.Kind() != reflect.Struct {
+		return "", nil, errors.New("input must be struct")
+	}
+
+	numFields := typ.NumField()
+	columns := make([]string, 0, numFields)
+	values := make([]any, 0, numFields)
+
+	for i := 0; i < numFields; i++ {
+		field := typ.Field(i)
+		columns = append(columns, field.Tag.Get("db"))
+		values = append(values, value.Field(i).Interface())
+	}
+
+	query := fmt.Sprintf("(%s) values (%s)",
+		quoteColumns(columns),
+		placeholders(len(columns)),
+	)
+
+	return query, values, nil
+}
+
+func quoteColumns(columns []string) string {
+	quotedColumns := make([]string, len(columns))
+	for i, col := range columns {
+		quotedColumns[i] = fmt.Sprintf(`"%s"`, col)
+	}
+	return strings.Join(quotedColumns, ", ")
+}
+
+func placeholders(n int) string {
+	return strings.Repeat("?, ", n-1) + "?"
 }
